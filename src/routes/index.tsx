b44a6274036a4a3b97f68e9544342d321cf3015d
Roel Bruggink <roel@jaroel.nl>
2024-03-28 10:43:46 +0100

    text-center the h1 for mobile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
-    <main class="flex flex-col items-center mx-auto text-gray-700 p-4">
+    <main class="flex flex-col items-center mx-auto text-center text-gray-700 p-4">
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
         Hello world!
       </h1>
