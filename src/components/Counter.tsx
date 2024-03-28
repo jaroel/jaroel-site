b8638b5b27f7014b33dd7f59d0a9356a0dc8fa7a
Roel Bruggink <roel@jaroel.nl>
2024-03-28 12:19:18 +0100

    Add some 'funny' prose for the button

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,13 +2,26 @@ import { createSignal } from "solid-js";
 
 export const [count, setCount] = createSignal(0);
 
+const messages = [
+  "Click me!",
+  "Once",
+  "Twice",
+  "Thrice",
+  "Quatre",
+  "🛥",
+  "Zes",
+  "Sieben",
+  "Acht",
+  "Negen",
+];
+
 export default function Counter() {
   return (
     <button
       class="w-[200px] rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]"
       onClick={() => setCount(count() + 1)}
     >
-      Clicks: {count()}
+      {messages[count()] ?? count()}
     </button>
   );
 }
