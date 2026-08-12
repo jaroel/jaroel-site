73e6a7a28738d77b72bef138eb7b4ad6713ff25b
Roel Bruggink <roel@jaroel.nl>
2026-08-12 21:45:23 +0200

    add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import { nitro } from "nitro/vite";
+import { solidStart } from "@solidjs/start/config";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+	plugins: [solidStart(), tailwindcss(), nitro()],
+	build: {
+		reportCompressedSize: false,
+	},
+	nitro: {
+		preset: "bun",
+		prerender: {
+			routes: ["/", "/resume", "/secret"],
+		},
+		routeRules: {
+			"/_build/assets/**": {
+				headers: { "cache-control": "max-age=31536000, immutable" },
+			},
+			"/_server/assets/**": {
+				headers: { "cache-control": "max-age=31536000, immutable" },
+			},
+			"/assets/**": {
+				headers: { "cache-control": "max-age=31536000, immutable" },
+			},
+		},
+	},
+});
