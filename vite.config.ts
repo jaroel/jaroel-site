be4c40cc69e806ebc944278dc0e322c1404dd206
Roel Bruggink <roel@jaroel.nl>
2026-08-25 17:20:21 +0200

    fix docker build

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,25 @@ import { solidStart } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	plugins: [solidStart(), tailwindcss(), nitro()],
-	build: {
-		reportCompressedSize: false,
-	},
-	nitro: {
-		preset: "bun",
-		prerender: {
-			routes: ["/", "/resume", "/secret"],
-		},
-		routeRules: {
-			"/_build/assets/**": {
-				headers: { "cache-control": "max-age=31536000, immutable" },
-			},
-			"/_server/assets/**": {
-				headers: { "cache-control": "max-age=31536000, immutable" },
-			},
-			"/assets/**": {
-				headers: { "cache-control": "max-age=31536000, immutable" },
-			},
-		},
-	},
+  plugins: [solidStart(), tailwindcss(), nitro()],
+  build: {
+    reportCompressedSize: false,
+  },
+  nitro: {
+    preset: "bun",
+    prerender: {
+      routes: ["/", "/resume", "/secret"],
+    },
+    routeRules: {
+      "/_build/assets/**": {
+        headers: { "cache-control": "max-age=31536000, immutable" },
+      },
+      "/_server/assets/**": {
+        headers: { "cache-control": "max-age=31536000, immutable" },
+      },
+      "/assets/**": {
+        headers: { "cache-control": "max-age=31536000, immutable" },
+      },
+    },
+  },
 });
